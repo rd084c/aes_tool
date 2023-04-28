52e7187a1ad563e9061d9c8fe9abaedbc9027d99
sunjun <sunjun@dehuinet.com>
2023-04-28 12:29:33 +0800

    去掉 warning

--- lib/aes_tool/aes_core.rb
+++ lib/aes_tool/aes_core.rb
@@ -15,7 +15,7 @@ class AesCore
   private
 
   def self.decrypt_data(encrypted_data, key, iv, cipher_type)
-    aes = OpenSSL::Cipher::Cipher.new(cipher_type)
+    aes = OpenSSL::Cipher.new(cipher_type)
     aes.decrypt
     aes.key = key
     aes.iv = iv if iv != nil
@@ -23,7 +23,7 @@ class AesCore
   end
 
   def self.encrypt_data(data, key, iv, cipher_type)
-    aes = OpenSSL::Cipher::Cipher.new(cipher_type)
+    aes = OpenSSL::Cipher.new(cipher_type)
     aes.encrypt
     aes.key = key
     aes.iv = iv if iv != nil
